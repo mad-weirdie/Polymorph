c42415074065543157848659328064454af30bed
AuroranAI <14842464+AuroranAI@users.noreply.github.com>
2021-04-26 00:02:49 -0700

    Character now moves relative to the camera instead of world

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,12 +16,18 @@ public class PlayerController : MonoBehaviour
     public float baseSpeed = 10f; //TODO: Character based speeds
     public float turnSpeed = 10f;
 
-    private Vector3 moveVec;
+    private Vector3 playerMoveInput;
     Quaternion m_Rotation;
     private Rigidbody rigidBody;
+
+    private Transform cameraTrans;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Camera obj
+        cameraTrans = Camera.main.transform;
+
         // Set all other game objects to not active.
         foreach (GameObject character in characters)
         {
@@ -45,17 +51,22 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        moveVec.Normalize();
+        playerMoveInput.Normalize();
         bool isWalking;
-        isWalking = !Mathf.Approximately(moveVec.x, 0f) || !Mathf.Approximately(moveVec.z, 0f);
+        isWalking = !Mathf.Approximately(playerMoveInput.x, 0f) || !Mathf.Approximately(playerMoveInput.z, 0f);
         activeAnims.SetBool("IsWalking", isWalking);
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, moveVec, turnSpeed * Time.deltaTime, 0f);
+        Vector3 moveDirection = cameraTrans.forward * playerMoveInput.z + cameraTrans.right * playerMoveInput.x;
+        moveDirection.y = 0f;
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, moveDirection, turnSpeed * Time.deltaTime, 0f);
         //print(desiredForward);
         m_Rotation = Quaternion.LookRotation(desiredForward);
         //print(m_Rotation);
         //Now, actually move!
-        rigidBody.MovePosition(rigidBody.position + moveVec*baseSpeed);
+        
+
+        rigidBody.MovePosition(rigidBody.position + moveDirection* baseSpeed);
         rigidBody.MoveRotation(m_Rotation);
 
     }
@@ -100,8 +111,8 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 vec = input.Get<Vector2>();
 
-        moveVec.x = vec.x;
-        moveVec.z = vec.y; //Y is height in 3d, but we want our y to handle movement on the Z plane.
+        playerMoveInput.x = vec.x;
+        playerMoveInput.z = vec.y; //Y is height in 3d, but we want our y to handle movement on the Z plane.
     }
 
     private void OnJump() {
