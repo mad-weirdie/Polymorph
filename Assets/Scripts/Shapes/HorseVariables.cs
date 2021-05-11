d57bd59854fb46f2ed4bd60cba5725bd6013b5e1
AuroranAI <14842464+AuroranAI@users.noreply.github.com>
2021-05-10 21:28:06 -0700

    Horse jump animation

--- Assets/Scripts/Shapes/HorseVariables.cs
+++ Assets/Scripts/Shapes/HorseVariables.cs
@@ -9,20 +9,28 @@ public class HorseVariables : ShapeVariables
 
     public float jumpForce = 7500f;
     protected Vector3 height;
+
+    private Animator horseAnimator;
     override public void Start()
     {
         base.Start();
         height = new Vector3(0f, jumpForce, 0f);
+        horseAnimator = GetComponent<Animator>();
+
     }
 
+
+  
     //Update is called once per frame
     override public void OnJump(bool isHeld)
     {
         //Actions that happen on the space bar being pressed, like jumping or flying.
-        if (Physics.Raycast(transform.position, Vector3.down, 1))
+        if (Physics.Raycast(transform.position, Vector3.down, .5f))
         {
             print("jump!");
             script.rigidBody.AddForce(height);
+            horseAnimator.Play("Jump");
+
         }
 
     }
