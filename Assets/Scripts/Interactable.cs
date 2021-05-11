d7a3cf47ef38679f7ad724ec0bda1dce213a256d
mad-weirdie <mwerries@uoregon.edu>
2021-05-11 02:48:39 -0700

    You can now press 'F' upon reaching the door to go to the other scene! :D

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Interactable : MonoBehaviour
+{
+    public string InteractText = "Press F to open the door";
+
+    Vector2 mousePosition;
+    bool inRange;
+    RaycastHit hit;
+    Ray ray;
+    public Camera mainCamera;
+    public TMP_Text text;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = InteractText;
+        text.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision interactable)
+    {
+        if (interactable.gameObject.CompareTag("Object"))
+        {
+            inRange = true;
+            text.gameObject.SetActive(true);
+        }
+    }
+
+    void OnCollisionExit(Collision interactable)
+    {
+        if (interactable.gameObject.CompareTag("Object"))
+        {
+            inRange = false;
+            text.gameObject.SetActive(false);
+        }
+    }
+
+    void OnGrab()
+    {
+        if (inRange)
+            SceneManager.LoadScene("Forest", LoadSceneMode.Single);
+    }
+}
